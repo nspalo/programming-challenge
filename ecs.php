c6027dfbb3e7ee965d1abefcd1cefa838baf1241
Noel Palo <noelspalo@gmail.com>
2021-08-26 23:01:50 +0800

    update ecs config - enable rule set - temporary skip some

--- ecs.php
+++ ecs.php
@@ -2,21 +2,16 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
-use PhpCsFixer\Fixer\Strict\StrictParamFixer;
-use PhpCsFixer\Fixer\Whitespace\IndentationTypeFixer;
-use PhpCsFixer\Fixer\Whitespace\LineEndingFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\CodingStandard\Fixer\Commenting\RemoveUselessDefaultCommentFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
 
-//    $containerConfigurator->import(SetList::PSR_12);
-//    $containerConfigurator->import(SetList::COMMON);
-//    $containerConfigurator->import(SetList::PHP_CS_FIXER);
     $containerConfigurator->import(SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::COMMON);
+//    $containerConfigurator->import(SetList::PSR_12);
+//    $containerConfigurator->import(SetList::PHP_CS_FIXER);
 
     $parameters = $containerConfigurator->parameters();
 
@@ -28,6 +23,21 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/tests',
     ]);
 
-//    $parameters->set(Option::SKIP, [
-//    ]);
+    $parameters->set(Option::SKIP, [
+        // Rule Set Skip from COMMON
+        PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
+        PhpCsFixer\Fixer\ClassNotation\NoBlankLinesAfterClassOpeningFixer::class,
+        PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer::class,                  // Want to use Yoda - Skip for now
+        PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class,   // Allow comments after code blocks
+        PhpCsFixer\Fixer\FunctionNotation\MethodArgumentSpaceFixer::class,
+        PhpCsFixer\Fixer\Phpdoc\NoEmptyPhpdocFixer::class,
+        PhpCsFixer\Fixer\Phpdoc\PhpdocSeparationFixer::class,
+        PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer::class,              // Let's Skip this for now
+        PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer::class,                  // Double -> Single, Skip for now
+        PhpCsFixer\Fixer\Whitespace\ArrayIndentationFixer::class,
+        PhpCsFixer\Fixer\Whitespace\LineEndingFixer::class,
+        PhpCsFixer\Fixer\Whitespace\NoWhitespaceInBlankLineFixer::class,
+        PhpCsFixer\Fixer\Whitespace\SingleBlankLineAtEofFixer::class,
+        Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer::class, // Skip for now
+    ]);
 };
